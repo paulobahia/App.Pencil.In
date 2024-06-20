3900d433bc7e3b5a5659b2b8a8b9ce0855411360
Paulo Bahia <pauloipatinga22@gmail.com>
2024-06-19 22:46:38 -0300

    Fix: eslint-disable

--- src/components/ui/phone-input.tsx
+++ src/components/ui/phone-input.tsx
@@ -44,16 +44,7 @@ const PhoneInput: React.ForwardRefExoticComponent<PhoneInputProps> =
           flagComponent={FlagComponent}
           countrySelectComponent={CountrySelect}
           inputComponent={InputComponent}
-          /**
-           * Handles the onChange event.
-           *
-           * react-phone-number-input might trigger the onChange event as undefined
-           * when a valid phone number is not entered. To prevent this,
-           * the value is coerced to an empty string.
-           *
-           * @param {E164Number | undefined} value - The entered value
-           */
-          onChange={(value) => onChange?.(value || "")}
+          onChange={(value) => onChange?.(value || "" as any)}
           {...props}
         />
       );
